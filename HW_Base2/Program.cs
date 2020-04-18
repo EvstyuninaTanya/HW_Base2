5ed00b4424d2573c5e1590bac0d1c2b2eaf40068
Tatiana <lady.evstyunina@mail.ru>
2020-04-18 21:29:25 +0300

    Добавьте файлы проекта.

---
+++ HW_Base2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW_Base2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "аЕавасатаюанаианаа";
+
+            int n = 0;
+
+            for (int i = 1; i < s.Length; i = i+2)
+            {
+                Console.Write(s[i]);
+            }
+
+        }
+    }
+}
